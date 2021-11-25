95a519e85bfb3b25e8441b04e31517c859b3904a
Lucas Moura <lucas.moura@meta.com.br>
2021-11-24 21:54:24 -0300

    feat: API implemented

---
+++ app/validators/DataValidator.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const Database = require(`../../database/infra/${process.env.DATABASE}/queries`)
+
+class DataValidator {
+  validationResult(validationName, validation) {
+    const validations = [
+      { 
+        name: 'storeData',
+        valid: !validation.length,
+        status: 400,
+        errorsOn: validation,
+        description: `Check if the following fields are empty: ${validation} .`
+      },
+      { 
+        name: 'updateData',
+        valid: !validation.length,
+        status: 400,
+        errorsOn: validation,
+        description: `Validation failed on fields: ${validation} .`
+      },
+      {
+        name: 'ERROR',
+        valid: false,
+        status: 500,
+        errorsOn: validation,
+        description: validation.message
+      }
+    ]
+
+    return validations.find(validation => validation.name === validationName)
+  }
+
+  async validateStoreData(tableName, notRequired, payload) { 
+    try {
+      const tableColumns = await Database.getColumns(tableName)
+      const payloadKeys = Object.keys(payload)
+      notRequired = Array.isArray(notRequired) ? notRequired : [notRequired]
+  
+      const validation = tableColumns.filter(column => {
+        if(!payloadKeys.includes(column) && !notRequired.includes(column)) return column
+        if(!String(payload[column]).length && !notRequired.includes(column)) return column
+      })
+
+      return this.validationResult('storeData', validation)
+    } catch(error) {
+      return this.validationResult('ERROR', error)
+    }
+  }
+
+  async validateUpdateData(tableName, notRequired, payload) {
+    let validation = []
+    let validationName = 'updateData'
+
+    await Database.getColumns(tableName)
+      .then(tableColumns => {
+        const payloadKeys = Object.keys(payload)
+        notRequired = Array.isArray(notRequired) ? notRequired : [notRequired]
+        
+        validation = payloadKeys.filter(key => {
+          if(!String(payload[key]).length && !notRequired.includes(key)) return key
+          if(!tableColumns.includes(key)) return key
+        })
+      })
+      .catch(error => {
+        validation = error
+        validationName = 'ERROR'
+      })
+
+    return this.validationResult(validationName, validation)
+  }
+}
+
+module.exports = new DataValidator
---
+++ database/migrations/services/migrationService.js
@@ -0,0 +1,83 @@
+const Database = require(`../../infra/${process.env.DATABASE}/queries`)
+
+class MigrationService {
+  constructor(migrationHistoryName) {
+    this.migrationHistoryName = migrationHistoryName
+  }
+
+  async 
+
+  async runEvent(migrationExists, migrationName) {
+    if(!migrationExists) {
+      const addToHistory = `
+        INSERT INTO migration_history (migration_name)
+        VALUES ($1)
+      `
+      await Database.raw(addToHistory, [migrationName])
+      console.log(migrationName)
+    }
+    return !migrationExists
+  }
+
+  async dropEvent(migrationExists, migrationName) {
+    if(migrationExists) {
+      const removeFormHistory = `
+        DELETE FROM migration_history 
+        WHERE migration_name = '${migrationName}'
+      `
+      console.log(migrationName)
+      await Database.raw(removeFormHistory)
+    }
+    return migrationExists
+  }
+
+  altEvents(migrationName, action) {
+    return !this.historyExists() && migrationName === this.migrationHistoryName && action === 'RUN'
+  }
+
+  routinePicker(action, migrationExists, migrationName) {
+    const routines = {
+      RUN: () => this.runEvent(migrationExists, migrationName),
+      DROP: () => this.dropEvent(migrationExists, migrationName),
+      ALTS: () => this.altEvents(migrationName, action),
+    }
+    if(this.historyExists() && this.migrationHistoryName !== migrationName) 
+      return routines[action](migrationExists)
+
+    return routines.ALTS(migrationName, action)
+  }
+  
+  async checkHistory(migrationName, action) {    
+    const migrationExists = await this.existsOnHistory(migrationName)
+    
+    return this.routinePicker(action, migrationExists, migrationName)
+  }
+
+  async existsOnHistory(migrationName) {
+    const checkIfMigration = `
+      SELECT id 
+      FROM migration_history 
+      WHERE migration_name = '${migrationName}'
+    `
+  
+    const { rows: historyRows } = await Database.raw(checkIfMigration)
+    const migrationExists = !!historyRows[0]
+
+    return migrationExists
+  }
+
+  async historyExists() {
+    const checkIfExists = `
+      SELECT EXISTS (
+        SELECT * FROM information_schema.tables 
+        WHERE table_name = 'migration_history'
+      );
+    `
+    const { rows: schemaRows } = await Database.raw(checkIfExists)
+    const { exists } = schemaRows[0]
+
+    return exists
+  }
+}
+
+module.exports = MigrationService
